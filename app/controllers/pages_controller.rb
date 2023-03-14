f2d9bee21de6f47c1e6095d947280567f6c7c700
saajida mathen <mathensaajida@gmail.com>
2023-03-14 15:32:31 +0200

    displays users on dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:dashboard]
 
-  def home
+  def dashboard
+    @users = User.all
   end
 end
