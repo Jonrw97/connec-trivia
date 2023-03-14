c00a6bdf42a57d6a9e45af81608214be2630244c
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2023-03-14 15:19:33 +0200

    added has_many to a few models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :friendships_as_asker, class_name: "Friendship", foreign_key: :asker_id
-  has_many :friendships_as_receiver, class_name: "Friendship", foreign_key: :receiver_id
-  has_many :assists_as_asker, class_name: "Assist", foreign_key: :asker_id
-  has_many :assists_as_receiver, class_name: "Assist", foreign_key: :receiver_id
+  has_many :friendships_as_asker, class_name: "Friendship", foreign_key: :asker_id, dependant: :destroy
+  has_many :friendships_as_receiver, class_name: "Friendship", foreign_key: :receiver_id, dependant: :destroy
+  has_many :assists_as_asker, class_name: "Assist", foreign_key: :asker_id, dependant: :destroy
+  has_many :assists_as_receiver, class_name: "Assist", foreign_key: :receiver_id, dependant: :destroy
 end
