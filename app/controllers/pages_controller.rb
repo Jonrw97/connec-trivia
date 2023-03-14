f496f8376544e6663bc6056ada729f3df30fbac4
saajida mathen <mathensaajida@gmail.com>
2023-03-14 17:18:10 +0200

    displays lifeline on dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,6 @@ class PagesController < ApplicationController
   def dashboard
     @users = User.all
     @score = current_user.choices.count { |choice| choice.correct }
+    @lifeline_count = current_user.lifeline_count
   end
 end
