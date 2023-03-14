def438ecbb66fe9f7b4ce209172308c1a67b70fd
saajida mathen <mathensaajida@gmail.com>
2023-03-14 15:49:03 +0200

    Dashboard done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:dashboard]
+  before_action :authenticate_user!, only: [:dashboard]
 
   def dashboard
     @users = User.all
