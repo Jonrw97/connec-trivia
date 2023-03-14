1035d2a42cabbd1e021656803dbaa3930a48097f
saajida mathen <mathensaajida@gmail.com>
2023-03-14 12:05:05 +0200

    finished routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :friendships, only: [:create]
+  end
+  resources :user_choices, only: [:create]
 
+  resource :questions, only: [:show] do
+    resources :assists, only: [:create]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
