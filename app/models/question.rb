8b1631f22b92410cb91c87b82b7c2f2603cc76ff
Jonathan Watkins <jonrw97@gmail.com>
2023-03-14 15:25:08 +0200

    added associations to models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ApplicationRecord
   enum difficulty: %i[easy medium hard]
   validates :prompt, presence: true
+  has_many :choices, dependent: :destroy
+  has_many :assists, dependent: :destroy
+  has_many :user_choices, through: :choices, dependent: :destroy
 end
