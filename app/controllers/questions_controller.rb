6222afcf5e0c52b992ee75ba763a1e049db26413
Jonathan Watkins <jonrw97@gmail.com>
2023-03-15 13:05:52 +0200

    fixed routes for questions so id in params

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,7 @@
 class QuestionsController < ApplicationController
   def show
-    @questions = current_user.unanswered_questions.sample
-    if @questions.nil?
-      redirect_to root_path
-    else
-      @choices = @questions.choices
-      @user_choice = UserChoice.new
-    end
+    @questions = Question.find(params[:id])
+    @choices = @questions.choices
+    @user_choice = UserChoice.new
   end
 end
--- app/controllers/user_choices_controller.rb
+++ app/controllers/user_choices_controller.rb
@@ -4,7 +4,11 @@ class UserChoicesController < ApplicationController
     user_choice.user = current_user
     user_choice.choice = Choice.find_by(content: safe_params[:choice_id])
     user_choice.save
-    redirect_to questions_path
+    if current_user.unanswered_questions.sample.nil?
+      redirect_to root_path
+    else
+      redirect_to question_path(current_user.unanswered_questions.sample)
+    end
   end
 
   private
