84403ec1dbfdda6a31b07b101554c238e25b8b74
Jonathan Watkins <jonrw97@gmail.com>
2023-03-15 15:19:44 +0200

    working on ajax post

--- app/javascript/controllers/correct_answers_controller.js
+++ app/javascript/controllers/correct_answers_controller.js
@@ -3,14 +3,18 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="correct-answers"
 export default class extends Controller {
   static targets = ["answers", "form"];
-  connect() {
-    console.log(this.element);
-    console.log(this.answersTarget);
-    console.log(this.formTarget);
-  }
+  connect() {}
   send(event) {
     event.preventDefault();
 
-    console.log("TODO: send request in AJAX");
+    fetch(this.formTarget.action, {
+      method: "POST",
+      headers: { Accept: "application/json" },
+      body: new FormData(this.formTarget),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
   }
 }
