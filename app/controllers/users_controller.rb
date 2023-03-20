0ddb79e794554cbbe9e1d47cc1e108025c85cc45
joshua singrew <rem28777@gmail.com>
2023-03-20 14:05:22 +0200

    cool-removed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,27 +3,28 @@ class UsersController < ApplicationController
 
     @score = current_user.score
     @answered = current_user.answered
-    total_a = 110
+    total_a = 120
     @total_a = total_a + @answered
-    total_correct = 2
+    total_correct = 60
     @total_correct = total_correct + @score
     @badges =[]
-    @badges << "🎖" if  @score == 10
+    @badges << "🎖" if  @score >= 10
     @badges << "👾" if  @total_correct == 100
     @badges << "🙀" if @total_correct >= 3
-    @badges << "☢︎" if @score == 0
+    @badges << "☢︎"  if @score == 0
     @badges << "💅🏽" if total_correct >= 1
-    @badges << "♖" if @total_a >= 20
+    @badges << "♖"  if @total_a >= 20
     @badges << "🎉" if @total_a >= 10
+
   end
+end
 
   def index
     if params[:query].present?
-      @users = User.where("user🆒name ILIKE ?", "%#{params[:query]}%")
+      @users = User.where("username ILIKE ?", "%#{params[:query]}%")
     else
       @users = []
 
     end
     @friendship = Friendship.new
   end
-end
