9aa956caa22a59988756534f0c0b94075886dccd
joshua singrew <rem28777@gmail.com>
2023-03-22 19:07:18 +0200

    badges-to-pictures

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 require 'pg_search'
 
 class User < ApplicationRecord
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
