14117d8f347c6a84d48bfee0aa76fea7cf5efc2b
Jonathan Watkins <91339335+Jonrw97@users.noreply.github.com>
2023-03-28 21:58:43 +0200

    Revert "refactor: some prettier stuff"

--- app/javascript/controllers/add_friendship_controller.js
+++ app/javascript/controllers/add_friendship_controller.js
@@ -1,26 +1,26 @@
-import { Controller } from '@hotwired/stimulus'
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="add-friendship"
 export default class extends Controller {
-  static targets = ['form', 'result']
+  static targets = ["form", "result"];
 
   connect() {}
 
   send(event) {
-    event.preventDefault()
-    console.log(this.formTarget)
-    console.log(this.formTarget.action)
+    event.preventDefault();
+    console.log(this.formTarget);
+    console.log(this.formTarget.action);
     fetch(this.formTarget.action, {
-      method: 'POST',
-      headers: { Accept: 'application/json' },
+      method: "POST",
+      headers: { Accept: "application/json" },
       body: new FormData(this.formTarget),
     })
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         if (data.inserted_item) {
-          this.resultTarget.insertAdjacentHTML('beforeend', data.inserted_item)
+          this.resultTarget.insertAdjacentHTML("beforeend", data.inserted_item);
         }
-        this.formTarget.classList.add('d-none')
-      })
+        this.formTarget.classList.add("d-none");
+      });
   }
 }
