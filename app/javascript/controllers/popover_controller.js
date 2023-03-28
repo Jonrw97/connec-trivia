fbb907c01c5568229e0d47be8673fde484d2485c
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-03-28 11:17:24 +0200

    chore: remove console.log

--- app/javascript/controllers/popover_controller.js
+++ app/javascript/controllers/popover_controller.js
@@ -4,7 +4,6 @@ import { Popover } from "bootstrap/dist/js/bootstrap.bundle.js";
 
 export default class extends Controller {
   connect() {
-    console.log("hello");
     new Popover(this.element);
   }
 }
