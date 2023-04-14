843dfb8b3962ca89d2aaec15f664809ca1a67a27
Jonathan Watkins <jonrw97@gmail.com>
2023-04-14 16:00:00 +0200

    added js controller with countdown timer to next day

---
+++ app/javascript/controllers/countdown_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="countdown"
+export default class extends Controller {
+  static targets = ["countdown"];
+
+  connect() {
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    tomorrow.setHours(0, 0, 0, 0);
+    this.secondsUntilEnd = tomorrow.getTime() / 1000 - today.getTime() / 1000;
+
+    const now = new Date().getTime();
+    this.endTime = new Date(now + this.secondsUntilEnd * 1000);
+
+    this.countdown = setInterval(this.countdown.bind(this), 250);
+  }
+
+  countdown() {
+    const now = new Date();
+    const secondsRemaining = (this.endTime - now) / 1000;
+
+    if (secondsRemaining <= 0) {
+      clearInterval(this.countdown);
+      this.countdownTarget.innerHTML = "Questions are Loading..";
+      return;
+    }
+
+    const secondsPerDay = 86400;
+    const secondsPerHour = 3600;
+    const secondsPerMinute = 60;
+    const hours = Math.floor((secondsRemaining % secondsPerDay) / secondsPerHour);
+    const minutes = Math.floor((secondsRemaining % secondsPerHour) / secondsPerMinute);
+    const seconds = Math.floor(secondsRemaining % secondsPerMinute);
+
+    this.countdownTarget.innerHTML = `${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+  }
+}
