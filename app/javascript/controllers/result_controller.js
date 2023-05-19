77ab11b00c27643f9be038420bff157ee71b74da
Jonathan Watkins <jonrw97@gmail.com>
2023-05-19 16:17:49 +0200

    stimulus controller working

--- app/javascript/controllers/result_controller.js
+++ app/javascript/controllers/result_controller.js
@@ -1,7 +1,12 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="result"
 export default class extends Controller {
-  connect() {
+  static targets = ["button", "list"];
+  connect() {}
+
+  toggle() {
+    this.buttonTarget.classList.toggle("d-none");
+    this.listTarget.classList.toggle("d-none");
   }
 }
