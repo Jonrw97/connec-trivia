e479a8633062d55b171626aac1bd7bf562c47b61
Jonathan Watkins <jonrw97@gmail.com>
2023-06-06 11:57:27 +0200

    yesterday partial and today and yesterday buttons added

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,7 @@ class Question < ApplicationRecord
   has_many :assists, dependent: :destroy
   has_many :user_choices, through: :choices, dependent: :destroy
   scope :today, -> { where(question_date: Date.today) }
+  scope :yesterday, -> { where(question_date: Date.yesterday) }
 
   def correct_choice
     choices.each { |c| return c if c.correct }
